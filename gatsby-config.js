1ec558cd43d0756c2b6c60e3e8c04d65522c2d27
Drew MacLean <arjmaclean@gmail.com>
2019-09-30 16:05:38 -0600

    Remove plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,12 +20,5 @@ module.exports = {
         path: `${__dirname}/src/media`,
       },
     },
-    {
-      resolve: `gatsby-plugin-netlify-functions`,
-      options: {
-        functionsSrc: `${__dirname}/src/functions`,
-        functionsOutput: `${__dirname}/functions`,
-      },
-    },
   ]
 }
