9515263c158634496ff2935cfd65ecf85b52a568
Drew MacLean <arjmaclean@gmail.com>
2019-10-21 08:54:41 -0600

    Add form validation

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,113 +1,122 @@
 import React from 'react'
-// import {
-//   Label,
-//   Input,
-//   Select,
-//   Textarea,
-//   Radio,
-//   Checkbox,
-// } from '@rebass/forms'
-// import { Heading, Flex, Box, Text, Button } from 'rebass';
 import {
   Container, Col, Form,
   FormGroup, Label, Input,
-  Button,
+  Button, FormFeedback
 } from 'reactstrap';
 import Fade from 'react-reveal/Fade'
 
 class ContactForm extends React.Component {
-  state = {
-    email: '',
-    message: '',
-    requestUpdates: '',
+  constructor(props) {
+    super(props);
+      this.state = {
+      email: '',
+      message: '',
+      requestUpdates: '',
+      validate: {
+        emailState: '',
+        messageState: '',
+      },
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  validateEmail(e) {
+    const emailRex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const { validate } = this.state
+      if (emailRex.test(e.target.value)) {
+        validate.emailState = 'valid'
+      } else {
+        validate.emailState = 'invalid'
+      }
+      this.setState({ validate })
+  }
+
+  validateMessage(e) {
+    const messageLengthMin = 10
+    const { validate } = this.state
+      if (e.target.value.length >= messageLengthMin) {
+        validate.messageState = 'valid'
+      } else {
+        validate.messageState = 'invalid'
+      }
+      this.setState({ validate })
+  }
+
+  handleChange = async (event) => {
+    const { target } = event;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const { name } = target;
+    await this.setState({
+      [ name ]: value,
+    });
   }
 
   render() {
+    const { email, message, requestUpdates } = this.state;
     return (
       <Fade left>
       <Container className="App">
         <h2>Contact Us</h2>
         <Form className="form" action="/submitForm" method="POST">
+          <Col>
             <FormGroup row>
               <Label>Email</Label>
-              <Col>
               <Input
                 type="email"
                 name="email"
                 id="email"
                 placeholder="example@email.com"
+                value={ email }
+                valid={ this.state.validate.emailState === 'valid' }
+                invalid={ this.state.validate.emailState === 'invalid' }
+                onChange={ (e) => {
+                            this.validateEmail(e)
+                            this.handleChange(e)
+                          } }
               />
-              </Col>
+              <FormFeedback>
+                Please input a valid email.
+              </FormFeedback>
             </FormGroup>
+            </Col>
+            <Col>
             <FormGroup row>
               <Label for="message">Message</Label>
-              <Col>
               <Input
                 type="textarea"
-                rows="10"
+                rows="6"
                 name="message"
                 id="message"
                 placeholder="Enter message here"
+                value={ message }
+                valid={ this.state.validate.messageState === 'valid' }
+                invalid={ this.state.validate.messageState === 'invalid' }
+                onChange={ (e) => {
+                            this.validateMessage(e)
+                            this.handleChange(e)
+                          } }
+
               />
-              </Col>
+              <FormFeedback>
+                Please input a more detailed message.
+              </FormFeedback>
             </FormGroup>
+            </Col>
+            <Col>
             <FormGroup row>
-              <Label for="requestUpdates">Receive Updates</Label>
-              <Col>
-              <Input
-                type="checkbox"
-                name="requestUpdates"
-                id="requestUpdates"
-              />
-              </Col>
+              <Label for="requestUpdates">I want to be the first to know about product updates!</Label>
+                  <Input
+                    type="checkbox"
+                    name="requestUpdates"
+                    id="requestUpdates"
+                  />
             </FormGroup>
-          <Button>Submit</Button>
+            </Col>
+          <Button disabled={!(this.state.validate.emailState === 'valid' && this.state.validate.messageState === 'valid')}>Submit</Button>
         </Form>
       </Container>
       </Fade>
-    //   <Box
-    //   as='form'
-    //   action="/submitForm" 
-    //   method="POST"
-    //   py={3}>
-    //   <Flex mx={-2} mb={3}>
-    //     <Box width={1/2} px={2}>
-    //       <Label htmlFor='email'>Email</Label>
-    //       <Input
-    //         id='email'
-    //         name='email'
-    //         type="email"
-    //         placeholder='example@email.com'
-    //       />
-    //     </Box>
-    //   </Flex>
-    //   <Flex mx={-2} mb={3}>
-    //     <Box width={1/2} px={2}>
-    //       <Label htmlFor='message'>Message</Label>
-    //       <Textarea
-    //       rows="10"
-    //       id='message'
-    //       name='message'
-    //       type='text'
-    //       placeholder='Enter message here'
-    //     />
-    //     </Box>
-    //   </Flex>
-    //   <Flex mx={-2} flexWrap='wrap'>
-    //     <Label width={[1/2, 1/4]} p={2}>
-    //       <Checkbox
-    //         id='requestUpdates'
-    //         name='requestUpdates'
-    //       />
-    //       Receive Updates
-    //     </Label>
-    //     <Box px={10} width={1/4}>
-    //       <Button bg='#008CBA'>
-    //         Submit
-    //       </Button>
-    //     </Box>
-    //   </Flex>
-    // </Box>
     );
   }
 }
